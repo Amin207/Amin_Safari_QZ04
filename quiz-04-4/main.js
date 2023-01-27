bf73a5eb951550172edce56f80dcfe75ce7e9081
AminS <safarin207@gmail.com>
2023-01-27 12:03:03 +0330

    Implement Q04

--- quiz-04-4/main.js
+++ quiz-04-4/main.js
@@ -1,8 +1,10 @@
-const tableHead = document.querySelector('thead');
-const tableBody = document.querySelector('tbody');
+const tableHead = document.querySelector("thead");
+const tableBody = document.querySelector("tbody");
+const showURL = document.getElementById("URL_SHOWUP");
 
 const renderTable = () => {
-	tableHead.innerHTML = `
+  showURL.innerHTML = `/quiz-04-1/index.html?${params.toString()}`;
+  tableHead.innerHTML = `
 		<tr>
 			<th scope="col">No.</th>
 			<th scope="col">Tour ID</th>
@@ -16,24 +18,58 @@ const renderTable = () => {
 			<th scope="col">Ratings Quantity</th>
 		</tr>`;
 
-	let rowCount = 1;
-	for (const tour of tours) {
-		tableBody.innerHTML += `
-			<tr>
-				<th scope="row">${rowCount}</th>
-				<td>${tour.id}</td>
-				<td>${tour.name}</td>
-				<td>${tour.location}</td>
-				<td>${tour.price}</td>
-				<td>${tour.maxGroupSize}</td>
-				<td>${tour.difficulty}</td>
-				<td>${tour.duration}</td>
-				<td>${tour.ratingsAverage}</td>
-				<td>${tour.ratingsQuantity}</td>
-			</tr>`;
-
-		rowCount += 1;
-	}
+  let rowCount = 1;
+  tours.forEach((tour) => {
+    console.log(handleConditions(tour));
+    if (handleConditions(tour)) {
+      tableBody.innerHTML += `
+				  <tr>
+					  <th scope="row">${rowCount}</th>
+					  <td>${tour.id}</td>
+					  <td>${tour.name}</td>
+					  <td>${tour.location}</td>
+					  <td>${tour.price}</td>
+					  <td>${tour.maxGroupSize}</td>
+					  <td>${tour.difficulty}</td>
+					  <td>${tour.duration}</td>
+					  <td>${tour.ratingsAverage}</td>
+					  <td>${tour.ratingsQuantity}</td>
+				  </tr>`;
+      rowCount += 1;
+    }
+  });
 };
+
+const params = new URLSearchParams(window.location.search);
+
+const paramsObj = () => {
+  let obj = {};
+  Array.from(params).forEach((e) => {
+    obj = { ...obj, [e[0]]: e[1] };
+  });
+  return obj;
+};
+
+const handleConditions = (tour) => {
+  if (Object.keys(paramsObj()).length > 0) {
+    let conditions;
+    Object.keys(tour).forEach((e) => {
+      if (
+        Object.keys(paramsObj()).includes(e).toString() &&
+        tour[e].toString() === paramsObj()[e]
+      ) {
+        conditions = conditions || tour[e].toString() === paramsObj()[e];
+      }
+    });
+    return conditions;
+  } else {
+    return tour;
+  }
+};
+
+console.log("**************");
+console.log(params.toString());
+console.log(paramsObj());
+console.log("**************");
 
 renderTable();
